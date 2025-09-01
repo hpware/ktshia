5b28b6f5ea3db991c826ed0e4a3a52dc0c34fc58
吳元皓 <hw@yuanhau.com>
2025-09-02 07:06:07 +0800

    wait what

--- backend-service/app/index.ts
+++ backend-service/app/index.ts
@@ -1,3 +1,5 @@
+import { getCachedData, saveCacheData } from "./ram_caching_layer";
+
 if (!process.env.AUTH_KEY) {
   console.error("Missing AUTH_KEY env value.");
   process.exit(1);
@@ -6,13 +8,26 @@ if (!process.env.TDX_API) {
   console.error("Missing TDX_API env value.");
   process.exit(1);
 }
-console.log(`Service started at port :${process.env.SERVICE_PORT || 4402}`);
+console.log(`Service started at port :${process.env.SERVICE_PORT || 4402} \n`);
+const enableLogTraffic = process.env.LOG_TRAFFIC || true;
 Bun.serve({
   port: process.env.SERVICE_PORT || 4402,
-  routes: {
-    "/": new Response("This ktshia backend service works :)"),
-  },
-  fetch(req) {
+  fetch(req, server) {
+    const url = new URL(req.url);
+    if (enableLogTraffic) {
+      console.log(
+        `[${new Date().toISOString()}] IP: ${server.requestIP(req).address} ${req.method} ${url.pathname}`,
+      );
+    }
+
+    if (url.pathname === "/") {
+      return new Response("This ktshia backend service works :)");
+    }
+
+    if (url.pathname.startsWith("/api/bus/")) {
+      return new Response("Hi");
+    }
+
     return new Response(
       JSON.stringify({
         error: "Not Found",
