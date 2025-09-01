5bc61afcea996831a3dd5cdb0c6c8333b3db8dd3
Howard <hw@yuanhau.com>
2025-09-01 20:09:25 +0800

    stuff

--- backend-service/app/index.ts
+++ backend-service/app/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+if (!process.env.AUTH_KEY) {
+    throw new Error("Missing AUTH_KEY");
+    process.exit(1);
+}
+Bun.serve({
+    port: 4402,
+    routes: {
+        "/": new Response("This ktshia backend service works :)"),
+    },
+    fetch(req) {
+        return new Response(JSON.stringify({
+            error: "Not Found",
+            status: 404
+        }), { status: 404, headers: { "Content-Type": "application/json" } });
+    },
+})
