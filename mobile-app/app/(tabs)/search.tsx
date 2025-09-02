eb9d7f27d1a7fc9434066b7f13b7646bd25895d3
吳元皓 <hw@yuanhau.com>
2025-09-03 07:41:49 +0800

    made some changes

--- mobile-app/app/(tabs)/search.tsx
+++ mobile-app/app/(tabs)/search.tsx
@@ -6,6 +6,7 @@ import {
   ScrollView,
   NativeSyntheticEvent,
   TextInputChangeEventData,
+  ActivityIndicator,
 } from "react-native";
 import { useState } from "react";
 
@@ -27,17 +28,20 @@ export default function SearchScreen() {
     city: "Taipei City",
   };
   const [searchBox, setSearchBox] = useState<string>();
+  const [isLoading, setIsLoading] = useState(false);
   const onSearchBoxChange = (
     e: NativeSyntheticEvent<TextInputChangeEventData>,
   ) => {
+    setIsLoading(true);
+    console.log(e.nativeEvent.text);
     setSearchBox(e.nativeEvent.text);
   };
   return (
     <View className="p-4">
       {/**Search bar */}
       <View className="mt-2 w-full flex flex-col">
-        <Text className="text-lg font-bold">查詢</Text>
-        <View className="flex flex-row w-full m-2">
+        <Text className="text-4xl font-bold m-4">查詢</Text>
+        <View className="flex flex-row w-[90%] m-auto">
           <TextInput
             placeholder={"Search"}
             className="rounded-xl border p-2 flex-1 bg-transparent"
@@ -46,17 +50,22 @@ export default function SearchScreen() {
           />
         </View>
       </View>
-      <Text>{searchBox}</Text>
-      <ScrollView>
-        <SearchContentItemBlock item={testJsonObject} />
-      </ScrollView>
+      {isLoading ? (
+        <ActivityIndicator size="large" className="self-center m-2 p-2" />
+      ) : (
+        <ScrollView className="pt-2 mt-2">
+          <SearchContentItemBlock item={testJsonObject} />
+          <SearchContentItemBlock item={testJsonObject} />
+          <SearchContentItemBlock item={testJsonObject} />
+        </ScrollView>
+      )}
     </View>
   );
 }
 
 function SearchContentItemBlock({ item }: { item: searchContentItem }) {
   return (
-    <View>
+    <View className="flex flex-col p-2 border m-2 rounded">
       <Text>{item.busName}</Text>
       <Text>
         {item.fromStop} -{">"} {item.toStop}
